aa6aad8a1c1cd1b124d81b2a49b9da9fb3629e23
Jorge Costa <jorgeefrazaocosta@gmail.com>
2018-01-03 00:28:31 +0000

    first commit

---
+++ InitialCoinOffering/InitialCoinOffering/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InitialCoinOffering
+//
+//  Created by Jorge Costa on 03/01/2018.
+//  Copyright © 2018 JorgeCosta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+
+}
+
